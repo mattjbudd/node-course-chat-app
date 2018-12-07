4014c13550260f75901397f257a68da3e5d7541f
BuildTools <unconfigured@null.spigotmc.org>
2018-12-07 16:23:00 -0700

    add geolocation support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message', () => {
@@ -12,3 +12,16 @@ describe('generateMessage', () => {
   });
 
 });
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from='Matt';
+    var lat = 4;
+    var lng = 5;
+    var url = 'https://www.google.com/maps?q=4,5';
+    var message = generateLocationMessage(from, lat, lng);
+    expect(message.url).toBeA('string');
+    expect(message.url).toBe(url);
+    expect(message.from).toBe('Matt');
+    expect(message.createdAt).toBeA('number');
+  });
+})
